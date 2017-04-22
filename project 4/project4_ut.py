2972023935b913384ea64a9910d6b2dbbdc4e6f5
romulomanzano <romulomanzano@gmail.com>
2017-04-22 13:51:27 -0400

    Pushing latest changes
    Randomized

--- project 4/project4_ut.py
+++ project 4/project4_ut.py
@@ -13,6 +13,9 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 from plotly.offline import plot as plot_off
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.cross_validation import train_test_split
+from sklearn.svm import SVR
+from sklearn.ensemble import ExtraTreesRegressor
 
 
 
@@ -46,56 +49,44 @@ test_file = 'test.csv'
 
 def convert_time(x):
     t = dt.datetime.strptime(x,"%m/%d/%Y %H:%M")
-    return pd.Series([t.weekday(),t.hour,t.year,t.month,t])
+    return pd.Series([t.weekday(),t.hour,t.year,t.month])
 
 
 target_columns = ['count','registered','casual']
-cat_cols = ['season','holiday','workingday','weekday','hr','yr','month']
 regress = ['count']
+regress_registered = ['registered']
+regress_casual = ['casual']
 
 raw_data = pd.read_csv(path+t_file)
 labels = raw_data[regress]
+labels_registered = raw_data[regress_registered]
+labels_casual = raw_data[regress_casual]
+
 data = raw_data.drop(target_columns,axis=1)
-data[['weekday','hr','yr','month','time']]  = data.apply(lambda x: convert_time(x['datetime']),axis=1)
+data[['weekday','hr','yr','month']]  = data.apply(lambda x: convert_time(x['datetime']),axis=1)
 
 
-plt = []
-weekdays = list(range(1,2))
-
-for d in weekdays:
-    line_x = data[data['weekday'] ==1]
-    line_y = labels[data['weekday'] ==1]
-    plt_x = go.Scatter(x=line_x['hr'],y=line_y['count'],mode = 'lines+markers')
-    plt.append(plt_x)
-    
-#py.plot(plt)
-plot_off(plt)
-
+cat_cols = ['season','holiday','workingday','weekday','hr','yr','month']
+binarize_cols = ['season','weather']#,'yr']
 
 data.drop('datetime',axis=1,inplace = True)
-data.drop('time',axis=1,inplace = True)
+#data.drop('time',axis=1,inplace = True)
 
-#print(data.dtypes)
-for col in cat_cols:
-    data[col] = data[col].astype('category')
-#print(data.dtypes)
+## binarizing?
+data = pd.get_dummies(data,columns = binarize_cols )
 
+#used 33
+train_data,test_data,train_labels ,test_labels = train_test_split(data, labels, test_size=0.20, random_state=42)
 
-train_data,test_data = data[:6000],data[6000:]
-train_labels, test_labels = labels[:6000], labels[6000:]
+t_l = np.array(test_labels['count'])
 
 #creating an adaBoostRegressor on this
-ada = AdaBoostRegressor()
-forest = RandomForestRegressor(n_estimators = 1000)
+forest = RandomForestRegressor(n_estimators = 1000,n_jobs = -1)
+extraTree = ExtraTreesRegressor(n_estimators = 1000,n_jobs = -1)
 
-m1 = ada.fit(train_data,train_labels)
-r = m1.predict(test_data)
-t_l = np.array(test_labels['count'])
-r_l = (np.array(r)).astype('int')
-ev =  rmsle(t_l,r_l)
+forest_reg = RandomForestRegressor(n_estimators = 1000,n_jobs = -1)
+forest_cas = RandomForestRegressor(n_estimators = 1000,n_jobs = -1)
 
-print('Accuracy (adaboost with decision trees):', m1.score(test_data, test_labels))
-print('RMSLE for adaboost',ev)
 
 f1 = forest.fit(train_data,train_labels)
 f_r = f1.predict(test_data)
@@ -105,22 +96,103 @@ print('Accuracy (random forest):', f1.score(test_data, test_labels))
 print('RMSLE with random forest:', ef)
 
 
-### Prepare for submission
-test_raw_data = pd.read_csv(path+test_file)
-test_raw_data [['weekday','hr','yr','month','time']]  = test_raw_data .apply(lambda x: convert_time(x['datetime']),axis=1)
+#Extra trees
+ex1 = extraTree.fit(train_data,train_labels)
+ex_r = ex1.predict(test_data)
+ex_l = (np.array(ex_r)).astype('int')
+ex =  rmsle(t_l,ex_l)
+print('Accuracy (extra Trees):', ex1.score(test_data, test_labels))
+print('RMSLE with extratrees:', ex)
+
+
+
+#composite:
+'''    
+train_labels_registered, test_labels_registered = labels_registered[:6000], labels_registered[6000:]
+train_labels_casual, test_labels_casual = labels_casual[:6000], labels_casual[6000:]
+'''
+
+
+data_comp = raw_data.drop(regress,axis=1)
+labels_comp = raw_data[regress]
+data_comp[['weekday','hr','yr','month']]  = data_comp.apply(lambda x: convert_time(x['datetime']),axis=1)
+
+
+cat_cols = ['season','holiday','workingday','weekday','hr','yr','month']
+
+
+data_comp.drop('datetime',axis=1,inplace = True)
+#data_comp.drop('time',axis=1,inplace = True)
+
+#print(data.dtypes)
 for col in cat_cols:
-    test_raw_data[col] = test_raw_data[col].astype('category')
-test_raw_data.drop('datetime',axis=1,inplace = True)
-test_raw_data.drop('time',axis=1,inplace = True)
+    data_comp[col] = data_comp[col].astype('category')
+#print(data.dtypes)
 
- 
+## binarizing?
+data_comp = pd.get_dummies(data_comp,columns = binarize_cols)
 
-baseline_r = f1.predict(test_raw_data)
-baseline_r_l = (np.array(baseline_r)).astype('int')
+#started with 33
+train_data_comp,test_data_comp,train_labels_comp,test_labels_comp = train_test_split(data_comp, labels_comp, test_size=0.20, random_state=42)
+
+train_labels_reg = train_data_comp[regress_registered]
+train_labels_cas = train_data_comp[regress_casual]
+
+train_data_comp.drop(regress_registered,axis=1,inplace=True)
+train_data_comp.drop(regress_casual,axis=1,inplace=True)
+test_data_comp.drop(regress_registered,axis=1,inplace=True)
+test_data_comp.drop(regress_casual,axis=1,inplace=True)
 
 
 
 
+f1_registered = forest_reg.fit(train_data_comp,train_labels_reg)
+f_r_registered = f1_registered.predict(test_data_comp)
 
 
+f1_casual = forest_cas.fit(train_data_comp,train_labels_cas)
+f_r_casual = f1_casual.predict(test_data_comp)
+
+f_r_composite = f_r_registered + f_r_casual
+f_r_composite_l = (np.array(f_r_composite)).astype('int')
+ef_composite =  rmsle(t_l,f_r_composite_l)
+print('RMSLE with random forest composite:', ef_composite)
+
+
+#averages
+f_r_avg = (f_r_composite_l + fr_l )/2
+f_r_avg_l = (np.array(f_r_avg)).astype('int')
+ef_avg =  rmsle(t_l,f_r_avg_l)
+print('RMSLE with random forest composite and full avg:', ef_avg)
+
+
+
+### Prepare for submission
+
+submit_raw_data = pd.read_csv(path+test_file)
+submit_raw_data [['weekday','hr','yr','month','time']]  = submit_raw_data .apply(lambda x: convert_time(x['datetime']),axis=1)
+for col in cat_cols:
+    submit_raw_data[col] = submit_raw_data[col].astype('category')
+    
+submit_result = (submit_raw_data['datetime']).to_frame()
+submit_result_full = (submit_raw_data['datetime']).to_frame()
+
+submit_raw_data.drop('datetime',axis=1,inplace = True)
+submit_raw_data.drop('time',axis=1,inplace = True)
+
+submit_raw_data = pd.get_dummies(submit_raw_data,columns = ['season','weather','yr'])
+
+
+submit_registered = f1_registered.predict(submit_raw_data)
+submit_casual = f1_casual.predict(submit_raw_data)
+
+submit_composite = submit_registered + submit_casual
+submit_composite_l = (np.array(submit_composite)).astype('int')
+
+submit_result['count'] = submit_composite_l
+
+#regular one
+submit_full = f1.predict(submit_raw_data)
+submit_full_l = (np.array(submit_full)).astype('int')
+submit_result_full['count'] = submit_full_l
 
